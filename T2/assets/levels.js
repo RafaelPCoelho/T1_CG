da90d4951a3c2279beda64fd12f388dc9e98e447
abreuthrj <abreuthrj@gmail.com>
2022-06-20 17:31:28 -0300

    documented levels

--- T2/assets/levels.js
+++ T2/assets/levels.js
@@ -1,7 +1,14 @@
+// Aqui são gerados os levels
 export default {
   1: {
     z: 0,
     enemies: [
+      /**
+       * Spawn de inimigos no mapa
+       * -- type
+       * air - Avião
+       * ground - Lança misseis terrestre
+       **/
       { type: "air", x: 0, z: -250, movement: "horz" },
       { type: "air", x: -30, z: -550 },
       { type: "air", x: 20, z: -850 },
@@ -25,6 +32,16 @@ export default {
       { type: "air", x: 5, z: -2600 },
       { type: "air", x: 35, z: -2700 },
       { type: "air", x: -50, z: -2800 },
+    ],
+    items: [
+      /**
+       * Drop de itens no mapa
+       * -- type
+       * health - Item que dá {value} de vida
+       * reloadSpeed - Item que dá maior velocidade de recarga por {value} segundos
+       */
+      { type: "health", x: 0, z: -100, value: 20 },
+      { type: "health", x: 0, z: -100, value: 20 },
     ],
   },
 };
